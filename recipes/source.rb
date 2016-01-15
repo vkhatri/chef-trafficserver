bd3f94d3fb54ce587e90b95f0cb37fefdf4d4e21
Bradley Corner <bcorner@fanatics.com>
2016-01-15 17:54:31 -0500

    Fixed the errors in recipes/source.rb:37:64: C: Space missing after comma. tarball_file = ::File.join(node['trafficserver']['version_dir'],"trafficserver-#{node['trafficserver']['version']}.tar.bz2")                                                                ^ recipes/source.rb:71:3: C: Do not use unless with else. Rewrite these with the positive case first.   unless defined?(v)   ^^^^^^^^^^^^^^^^^^
    1 file inspected, 2 offenses detected

--- recipes/source.rb
+++ recipes/source.rb
@@ -34,7 +34,7 @@ include_recipe 'trafficserver::user'
   end
 end
 
-tarball_file = ::File.join( node['trafficserver']['version_dir'], "trafficserver-#{node['trafficserver']['version']}.tar.bz2" )
+tarball_file = ::File.join(node['trafficserver']['version_dir'], "trafficserver-#{node['trafficserver']['version']}.tar.bz2")
 
 # Stop trafficserver Service if running for Version Upgrade
 service 'trafficserver' do
@@ -68,10 +68,10 @@ end
 
 compile_options = ''
 node['trafficserver']['compile'].each do |k, v|
-  unless defined?(v)
-    compile_options << " #{k}"
-  else
+  if defined?(v)
     compile_options << " #{k}=#{v}"
+  else
+    compile_options << " #{k}"
   end
 end
 
